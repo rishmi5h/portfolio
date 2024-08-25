8fb856d52494b632ae6e8b9863b04f28789c261f
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-26 03:15:20 +0530

    Create colors.tsx

---
+++ src/constants/colors.tsx
@@ -0,0 +1,6 @@
+export const darkModeColor = 'bg-gray-900';
+export const lightModeColor = 'bg-white';
+export const darkModeTextColor = 'text-white';
+export const lightModeTextColor = 'text-gray-900';
+export const navLinkColor = 'text-gray-900';
+export const navLinkHoverColor = '#F472B6';
