7fac8045dc5552f82221c9922ac4fea6eb2cbe95
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-26 02:29:16 +0530

    Create ThemeContext.tsx

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, ReactNode, useContext, useState } from 'react';
+
+type ThemeContextType = {
+  isDarkMode: boolean;
+  toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+};
