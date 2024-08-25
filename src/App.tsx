bb1ee8cb57e22f73aa1ed3f1945d31dfdef3013c
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-25 17:56:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,39 @@
-const Link = (props: JSX.IntrinsicElements['a']) => (
-  <a
-    className="text-pink-500 underline hover:no-underline dark:text-pink-400"
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Blogs from './pages/Blogs.tsx';
+import Contact from './pages/Contact.tsx';
+import Home from './pages/Home.tsx';
+import Projects from './pages/Projects.tsx';
+
+const NavLink = (props: JSX.IntrinsicElements['a'] & { to: string }) => (
+  <Link
+    className="text-gray-700 hover:text-pink-500 dark:text-gray-300 dark:hover:text-pink-400"
     {...props}
   />
 );
 
 export default function App() {
   return (
-    <div className="mx-auto my-8 mt-10 w-8/12 rounded border border-gray-200 p-4 shadow-md dark:border-neutral-600 dark:bg-neutral-800 dark:shadow-none">
-      <h1 className="mb-4 text-4xl">Welcome</h1>
-      <p className="my-4">
-        <em>Minimal, fast, sensible defaults.</em>
-      </p>
-      <p className="my-4">
-        Using <Link href="https://vitejs.dev/">Vite</Link>,{' '}
-        <Link href="https://reactjs.org/">React</Link>,{' '}
-        <Link href="https://www.typescriptlang.org/">TypeScript</Link> and{' '}
-        <Link href="https://tailwindcss.com/">Tailwind</Link>.
-      </p>
-      <p className="my-4">
-        Change{' '}
-        <code className="border-1 2py-1 rounded border border-pink-500 bg-neutral-100 px-1 font-mono text-pink-500 dark:border-pink-400 dark:bg-neutral-700 dark:text-pink-400">
-          src/App.tsx
-        </code>{' '}
-        for live updates.
-      </p>
-    </div>
+    <Router>
+      <div className="mx-auto my-8 mt-10 w-8/12">
+        <nav className="mb-6 flex justify-between">
+          <h1 className="text-2xl font-bold">
+            <NavLink to="/">rishmi5h</NavLink>
+          </h1>
+          <div className="space-x-10">
+            <NavLink to="/">home</NavLink>
+            <NavLink to="/projects">projects</NavLink>
+            <NavLink to="/blogs">blogs</NavLink>
+            <NavLink to="/contact">contact</NavLink>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<Projects />} path="/projects" />
+          <Route element={<Blogs />} path="/blogs" />
+          <Route element={<Contact />} path="/contact" />
+        </Routes>
+      </div>
+    </Router>
   );
 }
