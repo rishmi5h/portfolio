b8ada994c49c969b97570aa6b83d8a0d2f33dc11
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-27 02:14:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import About from './components/About.tsx';
 import Contact from './components/Contact.tsx';
+import CustomCursor from './components/CustomCursor.tsx';
 import Home from './components/Home.tsx';
 import Navbar from './components/Navbar.tsx';
 import Projects from './components/Projects.tsx';
-import { darkModeColor } from './constants/colors.tsx';
 import { ThemeProvider } from './contexts/ThemeContext.tsx';
 
 export default function App() {
   return (
     <ThemeProvider>
       <Router>
+        <CustomCursor />
         <Navbar />
         <Routes>
           <Route element={<Home />} path="/" />
