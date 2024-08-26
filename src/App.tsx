d65a49d4ded6557360b5db28f79e82ff3e573b85
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-27 01:40:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Blogs from './components/Blogs.tsx';
+import About from './components/About.tsx';
 import Contact from './components/Contact.tsx';
 import Home from './components/Home.tsx';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar.tsx';
 import Projects from './components/Projects.tsx';
 import { darkModeColor } from './constants/colors.tsx';
 import { ThemeProvider } from './contexts/ThemeContext.tsx';
@@ -11,15 +11,13 @@ export default function App() {
   return (
     <ThemeProvider>
       <Router>
-        {/* <div className={`min-h-screen bg-inherit transition-colors`}> */}
         <Navbar />
         <Routes>
           <Route element={<Home />} path="/" />
+          <Route element={<About />} path="/about" />
           <Route element={<Projects />} path="/projects" />
-          <Route element={<Blogs />} path="/blog" />
           <Route element={<Contact />} path="/contact" />
         </Routes>
-        {/* </div> */}
       </Router>
     </ThemeProvider>
   );
