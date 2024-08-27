c3f881d40a3646f3cd1e2fb9c1e26186c7c56403
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-28 01:20:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import About from './components/About.tsx';
 import Contact from './components/Contact.tsx';
 import CustomCursor from './components/CustomCursor.tsx';
+import Footer from './components/Footer.tsx';
 import Home from './components/Home.tsx';
 import Navbar from './components/Navbar.tsx';
 import Projects from './components/Projects.tsx';
@@ -19,6 +20,7 @@ export default function App() {
           <Route element={<Projects />} path="/projects" />
           <Route element={<Contact />} path="/contact" />
         </Routes>
+        <Footer />
       </Router>
     </ThemeProvider>
   );
