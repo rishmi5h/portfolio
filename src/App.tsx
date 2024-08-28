b2bf34b6214face5a506ea3cb34bc776835ad08f
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-08-29 03:01:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Contact from './components/Contact.tsx';
 import CustomCursor from './components/CustomCursor.tsx';
 import Footer from './components/Footer.tsx';
 import Home from './components/Home.tsx';
+import Interesting from './components/Interesting.tsx';
 import Navbar from './components/Navbar.tsx';
 import Projects from './components/Projects.tsx';
 import { ThemeProvider } from './contexts/ThemeContext.tsx';
@@ -12,15 +13,20 @@ export default function App() {
   return (
     <ThemeProvider>
       <Router>
-        <CustomCursor />
-        <Navbar />
-        <Routes>
-          <Route element={<Home />} path="/" />
-          <Route element={<About />} path="/about" />
-          <Route element={<Projects />} path="/projects" />
-          <Route element={<Contact />} path="/contact" />
-        </Routes>
-        <Footer />
+        <div className="flex min-h-screen flex-col">
+          <CustomCursor />
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route element={<Home />} path="/" />
+              <Route element={<About />} path="/about" />
+              <Route element={<Projects />} path="/projects" />
+              <Route element={<Contact />} path="/contact" />
+              <Route element={<Interesting />} path="/interesting" />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
       </Router>
     </ThemeProvider>
   );
