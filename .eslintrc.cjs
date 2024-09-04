27545e99ede87e533c06bd7d6df69ea34debc58d
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-05 01:20:19 +0530

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,14 @@
 module.exports = {
   extends: ['@nkzw'],
+  rules: {
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        ignoreCase: true,
+        reservedFirst: true,
+      },
+    ],
+  },
 };
