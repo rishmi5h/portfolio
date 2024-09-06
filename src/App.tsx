a2361caacc5330bcc6540b9143c2340775bb6892
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-07 03:00:43 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,10 @@ export default function App() {
           />
           <meta content="index, follow" name="robots" />
           <link href="https://rishmi5h.com" rel="canonical" />
+          <link
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>"
+            rel="icon"
+          />
         </Helmet>
         <div className="flex min-h-screen flex-col">
           <CustomCursor />
