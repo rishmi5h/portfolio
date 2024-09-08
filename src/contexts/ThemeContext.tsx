9fe5ae2f0091f0e8c884bcf87976b0eb5ca5c5bf
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-08 16:37:05 +0530

    fix(theme): on refresh to keep the theme

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, ReactNode, useContext, useState } from 'react';
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
 type ThemeContextType = {
   isDarkMode: boolean;
@@ -10,7 +16,14 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme ? savedTheme === 'dark' : true;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+  }, [isDarkMode]);
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
