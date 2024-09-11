f859e61c2bb8947643c3ce9abfe6354ff327392a
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-12 05:13:40 +0530

    Create timelineEvents.tsx

---
+++ src/constants/timelineEvents.tsx
@@ -0,0 +1,28 @@
+export const timelineEvents = [
+  {
+    description:
+      'Developed a quick build tool that reduced development time by 30%. Trained other developers on new technologies / architectures.',
+    location: 'Hyderabad, India',
+    skill:
+      'java, springboot, react, docker, airflow, typescript, tailwind, python',
+    title: 'Software Engineer - Optum',
+    year: '2021/10 - Current',
+  },
+  {
+    description:
+      'Build incident management webapp using React and collaborated with backend developers.',
+    location: 'Remote',
+    skill: 'react, html, css, javascript, tailwind',
+    title: 'Software Engineer Trainee - Incture Technologies',
+    year: '2021/06 - 2021/09',
+  },
+  {
+    description:
+      'Specialization in Electrical Engineering, did internships, projects and organized many events.',
+    location: 'Raipur, India',
+    skill:
+      'electrical engineering, sponsorship, event-management, machine learning',
+    title: 'B.Tech - NIT Raipur',
+    year: '2017/08 - 2021/05',
+  },
+];
