b6991a305546ccdc63f688cfcc2b1bc505e10e6a
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-13 20:26:33 +0530

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,74 +1,11 @@
 import React, { useEffect } from 'react';
 import ProjectCard from '../components/ProjectCard.tsx';
 import { darkModeColor, lightModeColor } from '../constants/colors.tsx';
+import { projects } from '../constants/projects.tsx';
 import { useTheme } from '../contexts/ThemeContext.tsx';
 
 const Projects: React.FC = () => {
   const { isDarkMode } = useTheme();
-
-  const projects = [
-    {
-      description:
-        'Imagery is a image management service that allows you to upload, store and edit your images. ',
-      githubLink: 'https://github.com/rishmi5h/image-processing-service',
-      imageUrl: 'assets/imagery.webp',
-      name: 'Imagery - Image management',
-      projectLink: 'https://imagery.rishmi5h.com/',
-      techStack: ['Spring Boot', 'React', 'Tailwind CSS', 'MySQL', 'AWS S3'],
-    },
-    {
-      description: 'Portfolio website but themed like a terminal. ',
-      githubLink: 'https://github.com/rishmi5h/terminal',
-      imageUrl: 'assets/terminal.webp',
-      name: 'Terminal - Portfolio',
-      projectLink: 'https://terminal.rishmi5h.com/',
-      techStack: ['React', 'Tailwind CSS', 'TypeScript', 'Vite', 'Netlify'],
-    },
-    {
-      description:
-        'ΣUI is a Component Library that helps in building UI faster and easier. ',
-      githubLink: 'https://github.com/rishmi5h/Sigma-UI',
-      imageUrl: 'assets/sigma-ui.webp',
-      name: 'Sigma UI - Component Library',
-      projectLink: 'https://sigma-ui.rishmi5h.com/',
-      techStack: ['HTML', 'CSS', 'JavaScript', 'Tailwind CSS'],
-    },
-    {
-      description: 'A fun website to ask your crush out on a date. ',
-      githubLink: 'https://github.com/rishmi5h/doyouwannagooutwithme',
-      imageUrl: 'assets/do-you-wanna-go-out.webp',
-      name: 'Do you wanna go out with me?',
-      projectLink: 'https://doyouwannagooutwith.rishmi5h.com/',
-      techStack: ['HTML', 'CSS', 'JavaScript'],
-    },
-    {
-      description: 'Created 30 projects in 30 days using vanilla JavaScript. ',
-      githubLink: 'https://github.com/rishmi5h/30days-vanilla-js-challenge',
-      imageUrl: 'assets/30-days-vanilla-js.webp',
-      name: '30 days of vanilla JavaScript',
-      projectLink: 'https://vanilla-js-projects-101.netlify.app/',
-      techStack: ['HTML', 'CSS', 'JavaScript'],
-    },
-    {
-      description:
-        'Long short term memory (LSTM) based load forecasting for electricity demand',
-      githubLink: 'https://github.com/rishmi5h/final-year-project',
-      imageUrl: 'assets/lstm.webp',
-      name: 'LSTM based load forecasting',
-      projectLink:
-        'https://drive.google.com/file/d/1nBQQUfHQqdP92OzcVOUmt2RFXrVNy6Sf/view',
-      techStack: ['Machine Learning', 'Python', 'LSTM'],
-    },
-    {
-      description: 'A fun game to test your knowledge of flags emojis.',
-      githubLink: 'https://github.com/rishmi5h/know-the-flags',
-      imageUrl: 'assets/know-the-flags.webp',
-      name: 'Know The Flags',
-      projectLink: 'https://flags-emoji.netlify.app/',
-      techStack: ['React', 'Tailwind CSS'],
-    },
-    // Add more projects as needed
-  ];
 
   useEffect(() => {
     projects.forEach((project) => {
@@ -97,13 +34,13 @@ const Projects: React.FC = () => {
     >
       <div className="container mx-auto px-4">
         <h1
-          className={`mb-12 text-center text-4xl font-bold ${
+          className={`my-12 text-center text-4xl font-bold ${
             isDarkMode ? 'text-white' : 'text-gray-800'
           }`}
         >
           my projects
         </h1>
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+        <div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
           {projects.map((project, index) => (
             <ProjectCard key={index} {...project} />
           ))}
